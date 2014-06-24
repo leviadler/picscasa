d2dc13e1cb8c57e1c0c7d12f808f260c81589617
Levi Adler <leviadler@gmail.com>
2014-06-24 14:39:14 -0400

    complete basic tests on User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ describe User do
   it { should ensure_length_of(:password).is_at_least(6) }
   it { should validate_confirmation_of(:password) }
   it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:password_digest).with_message("Password can't be blank") }
   it { should validate_presence_of(:fname) }
   it { should validate_presence_of(:lname) }
   it { should validate_uniqueness_of(:email) }
