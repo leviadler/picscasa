bcdc7f7f8b0d0f328c5590ca3a3fe5632107f7b0
Levi Adler <leviadler@gmail.com>
2014-06-25 01:10:34 -0400

    add before action to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,32 @@
 class SessionsController < ApplicationController
-  
+
+  before_action :require_signed_out, only: [:new, :create]
+
   def new
-    
+
   end
-  
+
   def create
     @user = User.find_by_credentials(
       user_params[:email],
       user_params[:password]
     )
-    
+
     if @user
       login!(@user)
       redirect_to user_url(@user)
-    else 
+    else
       flash.now[:error] = "Invalid email or password"
       render :new
     end
-    
+
   end
-  
+
   def destroy
     logout!
     redirect_to new_session_url
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:email, :password)
