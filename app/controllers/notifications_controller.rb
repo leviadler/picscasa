705d9a9ce3676451c42706fcd6b6469eb83e62bf
Levi Adler <leviadler@gmail.com>
2014-06-26 01:50:56 -0400

    move header and footer to partial

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,18 +1,19 @@
 class NotificationsController < ApplicationController
-  
+
   before_action :require_signed_in
   after_action :mark_as_read
-  
+
   def index
+    # should these methods be moved to users model?
     @notifications = current_user.notifications.where(is_unread: true)
-    
+
     @old_notifications = current_user.notifications.where(is_unread: false).limit(20)
   end
-  
+
   def update
-    
+
   end
-  
+
   private
   def mark_as_read
     @notifications.update_all(is_unread: false)
