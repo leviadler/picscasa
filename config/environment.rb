0e1847fae0a861b5d4371eb7df872921b8bed282
App Academy Student <student@appacademy.io>
2014-06-26 17:47:24 -0400

    change email settings, again

--- config/environment.rb
+++ config/environment.rb
@@ -3,20 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-if Rails.env.production?
-  # only send real emails in production; use Sengrid
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
-  }
-  ActionMailer::Base.delivery_method ||= :smtp
-  config.action_mailer.default_url_options = { host: 'picscasa.herokuapp.com' }
-elsif Rails.env.development?
-  ActionMailer::Base.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-end
