466acb3b4c2b95509bc5885daed52494b9c22686
Levi Adler <leviadler@gmail.com>
2014-07-06 13:05:33 -0400

    style photo delete button

--- app/assets/javascripts/views/photos/photo_tile.js
+++ app/assets/javascripts/views/photos/photo_tile.js
@@ -1,24 +1,26 @@
 Picscasa.Views.PhotoTile = Backbone.View.extend({
   template: JST["photos/photo_tile"],
-  
+
   events: {
     "click .delete-photo": "deletePhoto"
   },
-  
+
   tagName: "li",
-  
+
   render: function() {
     var renderedContent = this.template({
       photo: this.model
     });
-    
+
     this.$el.html(renderedContent);
     return this;
   },
-  
+
   deletePhoto: function(event) {
     event.preventDefault();
-    this.model.destroy();
-    this.remove();
+    if(confirm("Are you sure you want to delete this photo?")) {
+      this.model.destroy();
+      this.remove();
+    }
   }
 })
